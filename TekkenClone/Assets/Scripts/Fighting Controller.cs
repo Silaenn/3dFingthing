05fd5226d3f12b626e82619337c8a9723f4d5e24
Deo silaen <deokeldisilaen@gmail.com>
2024-12-12 12:28:21 +0700

    addMove

---
+++ TekkenClone/Assets/Scripts/Fighting Controller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 10f;
+    private CharacterController characterController;
+    private Animator animator;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        PerformMovement();
+    }
+
+    void PerformMovement(){
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(-verticalInput, 0f, horizontalInput);
+
+        if(movement != Vector3.zero){
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if(horizontalInput > 0){
+                animator.SetBool("Walking", true);
+            } else if(horizontalInput < 0){
+                animator.SetBool("Walking", true);
+            } else if (verticalInput != 0){
+                animator.SetBool("Walking", true);
+            }
+        } else {
+            animator.SetBool("Walking", false);
+        }
+
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
+    }
+
+}
