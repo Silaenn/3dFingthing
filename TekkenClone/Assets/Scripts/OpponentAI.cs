596aebeb0cad582ca7e7308360768d8ba5682537
Deo silaen <deokeldisilaen@gmail.com>
2024-12-13 14:22:13 +0700

    addEnemyController

--- TekkenClone/Assets/Scripts/OpponentAI.cs
+++ TekkenClone/Assets/Scripts/OpponentAI.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OpponentAI : MonoBehaviour
@@ -8,8 +6,9 @@ public class OpponentAI : MonoBehaviour
     [Header("Opponent Movement")]
     public float movementSpeed = 1f;
     public float rotationSpeed = 10f;
-    private CharacterController characterController;
-    private Animator animator;
+    public CharacterController characterController;
+    public Animator animator;
+
     [Header("Opponent Fight")]
     public float attackCooldown = 0.5f;
     public int attackDamages = 5;
@@ -28,4 +27,55 @@ public class OpponentAI : MonoBehaviour
     public ParticleSystem attack2Effect;
     public ParticleSystem attack3Effect;
     public ParticleSystem attack4Effect;
+
+
+    private void Awake() {
+        createRandomNumber();
+    }
+
+    private void Update() {
+        for(int i = 0; i < fightingControllers.Length; i++){
+            if(players[i].gameObject.activeSelf){
+                Vector3 direction = (players[i].position - transform.position).normalized;
+                characterController.Move(direction * movementSpeed * Time.deltaTime);
+
+                Quaternion targetRotattion = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotattion, rotationSpeed * Time.deltaTime);
+
+                animator.SetBool("Walking", true);
+            }
+        }
+    }
+
+     void PerformAttack(int attackIndex){
+         animator.Play(attackAnimations[attackIndex]);
+
+         int damage = attackDamages;
+         Debug.Log("Performed attack: " + attackIndex + 1 + " dealing " + damage + "damage");
+
+         lastAttackTime = Time.time;
+
+    }
+    void PerformDodgeFront(){
+        animator.Play("DodgeFrontAnimation");
+
+        Vector3 dodgeDIrection = -transform.forward * dodgeDistance;
+
+        characterController.SimpleMove(dodgeDIrection);
+    }
+    void createRandomNumber(){
+        randomNumber = UnityEngine.Random.Range(1, 5);
+    }
+    public void Attack1Effect(){
+        attack1Effect.Play();
+    }
+    public void Attack2Effect(){
+        attack2Effect.Play();
+    }
+    public void Attack3Effect(){
+        attack3Effect.Play();
+    }
+    public void Attack4Effect(){
+        attack4Effect.Play();
+    }
 }
